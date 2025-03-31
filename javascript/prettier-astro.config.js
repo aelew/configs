3258873e41a95da23111c3090c10f33e92a7d00d
Andre Lew <me@aelew.com>
2025-03-30 17:25:24 -0700

    update astro prettier config

--- javascript/prettier-astro.config.js
+++ javascript/prettier-astro.config.js
@@ -1,10 +1,15 @@
 /** @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
-const config = {
+export default {
   importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
   importOrderParserPlugins: ['typescript', 'decorators-legacy'],
   astroAllowShorthand: true,
   trailingComma: 'none',
   singleQuote: true,
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports'
+  ],
   overrides: [
     {
       files: '*.astro',
@@ -12,12 +17,5 @@ const config = {
         parser: 'astro'
       }
     }
-  ],
-  plugins: [
-    'prettier-plugin-astro',
-    'prettier-plugin-tailwindcss',
-    '@ianvs/prettier-plugin-sort-imports'
   ]
 };
-
-module.exports = config;
